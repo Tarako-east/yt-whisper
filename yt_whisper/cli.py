027efb3edfdd2ba841279545dbbd2073d700a0bd
suzuna <suzuna@example.com>
2022-11-18 17:11:53 +0900

    add format txt

--- yt_whisper/cli.py
+++ yt_whisper/cli.py
@@ -4,7 +4,7 @@ from whisper.tokenizer import LANGUAGES, TO_LANGUAGE_CODE
 import argparse
 import warnings
 import yt_dlp
-from .utils import slugify, str2bool, write_srt, write_vtt
+from .utils import slugify, str2bool, write_srt, write_vtt, write_txt
 import tempfile
 
 
@@ -16,7 +16,7 @@ def main():
     parser.add_argument("--model", default="small",
                         choices=whisper.available_models(), help="name of the Whisper model to use")
     parser.add_argument("--format", default="vtt",
-                        choices=["vtt", "srt"], help="the subtitle format to output")
+                        choices=["vtt", "srt", "txt"], help="the subtitle format to output")
     parser.add_argument("--output_dir", "-o", type=str,
                         default=".", help="directory to save the outputs")
     parser.add_argument("--verbose", type=str2bool, default=False,
@@ -26,7 +26,7 @@ def main():
     parser.add_argument("--language", type=str, default=None, choices=sorted(LANGUAGES.keys()) + sorted([k.title() for k in TO_LANGUAGE_CODE.keys()]),
                         help="language spoken in the audio, skip to perform language detection")
 
-    parser.add_argument("--break-lines", type=int, default=0, 
+    parser.add_argument("--break-lines", type=int, default=0,
                         help="Whether to break lines into a bottom-heavy pyramid shape if line length exceeds N characters. 0 disables line breaking.")
 
     args = parser.parse_args().__dict__
@@ -52,13 +52,22 @@ def main():
         if (subtitles_format == 'vtt'):
             vtt_path = os.path.join(output_dir, f"{slugify(title)}.vtt")
             with open(vtt_path, 'w', encoding="utf-8") as vtt:
-                write_vtt(result["segments"], file=vtt, line_length=break_lines)
+                write_vtt(result["segments"], file=vtt,
+                          line_length=break_lines)
 
             print("Saved VTT to", os.path.abspath(vtt_path))
+        elif (subtitles_format == 'txt'):
+            txt_path = os.path.join(output_dir, f"{slugify(title)}.txt")
+            with open(txt_path, 'w', encoding="utf-8") as txt:
+                write_txt(result["segments"], file=txt,
+                          line_length=break_lines)
+
+            print("Saved txt to", os.path.abspath(txt_path))
         else:
             srt_path = os.path.join(output_dir, f"{slugify(title)}.srt")
             with open(srt_path, 'w', encoding="utf-8") as srt:
-                write_srt(result["segments"], file=srt, line_length=break_lines)
+                write_srt(result["segments"], file=srt,
+                          line_length=break_lines)
 
             print("Saved SRT to", os.path.abspath(srt_path))
 
